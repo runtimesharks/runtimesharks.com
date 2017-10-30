0e537c89a3fb7e1e24053dbf5a98a83a242a0b0f
Roland Leth <roland@leth.ro>
2017-10-30 11:15:24 +0200

    Improved paddings, added DeinDeal

--- Sources/App/Controllers/HomeController.swift
+++ Sources/App/Controllers/HomeController.swift
@@ -8,34 +8,8 @@ struct HomeController {
 	// MARK: - Routes
 
 	static func show(with request: Request) throws -> ResponseRepresentable {
-		let path = "/images/project-screenshots"
-		
-		let projects = [
-			[
-				"name": "Carminder",
-				"description": "Helps you keep track of your car's maintenance. See all your car's events at a glance and easily add a reminder, and how often you'd like to be reminded. [Discontinued as of 2014]",
-				"image": "\(path)/carminder.png",
-				"imagePosition": "left",
-				"themeColor": "#00A29B"
-			],
-			[
-				"name": "Expenses Planner",
-				"description": "Helps you keep track of your upcoming expenses. Easily set up your upcoming expenses, their recurring intervals, a reminder, and how often you'd like to be reminded. [Discontinued as of 2014]",
-				"image": "\(path)/expenses-planner.png",
-				"imagePosition": "right",
-				"themeColor": "#9A7CDC"
-			],
-			[
-				"name": "Puppet Anthems",
-				"description": "The fun way to learn about national anthems, for adults and kids alike. All the lyrics, all the music, all the history, and all the info related to the 32 countries from the 2014 World Cup in one great app.",
-				"image": "\(path)/puppet-anthems.png",
-				"imagePosition": "left",
-				"themeColor": "#A91F13"
-			]
-		]
-		
 		let parameters = [
-			"projects": projects
+			"projects": Project.all()
 		]
 		
 		return try drop.view.make("home", with: parameters, for: request)
---
+++ Sources/App/Models/Project.swift
@@ -0,0 +1,81 @@
+//
+//  Project.swift
+//  App
+//
+//  Created by Roland Leth on 30.10.2017.
+//
+
+import Vapor
+
+struct Project: NodeRepresentable {
+	
+	let name: String
+	let description: String
+	let image: String
+	var imagePosition: String
+	let color: String
+	let link: String
+	
+	func makeNode(in context: Context?) throws -> Node {
+		let mirror = Mirror(reflecting: self)
+		var node: [String: String] = [:]
+		
+		for child in mirror.children {
+			guard let label = child.label else { continue }
+			guard let value = child.value as? String else { continue }
+			node[label] = value
+		}
+		
+		return try node.makeNode(in: context)
+	}
+	
+	init(name: String, description: String, image: String, color: String, link: String) {
+		self.name = name
+		self.description = description
+		self.image = image
+		imagePosition = "left"
+		self.color = color
+		self.link = link
+	}
+	
+}
+
+extension Project {
+	
+	static func all() -> [Project] {
+		let path = "/images/project-screenshots"
+		var projects = [
+			Project(
+				name: "DeinDeal",
+				description: "Between 2013 and 2017 Roland has worked for DeinDeal, one of the dominating flash-sales companies in Switzerland, and starting with 2014 he took the role of iOS Lead.",
+				image: "\(path)/deindeal.png",
+				color: "#C40E3D",
+				link: "https://itunes.apple.com/us/app/deindeal/id465657999?ls=1&mt=8"),
+			Project(
+				name: "Carminder",
+				description: "Helps you keep track of your car's maintenance. See all your car's events at a glance and easily add a reminder, and how often you'd like to be reminded. [Discontinued as of 2014]",
+				image: "\(path)/carminder.png",
+				color: "#00A29B",
+				link: "/projects/carminder"),
+			Project(
+				name: "Expenses Planner",
+				description: "Helps you keep track of your upcoming expenses. Easily set up your upcoming expenses, their recurring intervals, a reminder, and how often you'd like to be reminded. [Discontinued as of 2014]",
+				image: "\(path)/expenses-planner.png",
+				color: "#481FAF",
+				link: "/projects/expenses-planner"),
+			Project(
+				name: "Puppet Anthems",
+				description: "The fun way to learn about national anthems, for adults and kids alike. All the lyrics, all the music, all the history, and all the info related to the 32 countries from the 2014 World Cup in one great app.",
+				image: "\(path)/puppet-anthems.png",
+				color: "#A91F13",
+				link: "http://puppetanthems.com")
+		]
+		
+		for project in projects.enumerated() {
+			projects[project.offset].imagePosition = project.offset % 2 == 0 ? "left" : "right"
+		}
+		
+		return projects
+	}
+	
+}
