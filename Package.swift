5344a00bcab5f050873bcb176053dc96977ec2d7
Roland Leth <roland@leth.ro>
2018-07-09 14:06:33 +0300

    Fixed targets and config

--- Package.swift
+++ Package.swift
@@ -3,23 +3,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "RuntimeSharks",
-    products: [
-        .executable(name: "App", targets: ["App"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.4.0")),
-        .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0"))
-    ],
-    targets: [
-        .target(name: "App", dependencies: ["Vapor", "LeafProvider"],
-               exclude: [
-                   "Config",
-                   "Database",
-                   "Public",
-                   "Resources"
-               ]),
-        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
-    ]
+	name: "RuntimeSharks",
+	products: [
+		.executable(name: "App", targets: ["App"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.4.0")),
+		.package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0"))
+	],
+	targets: [
+		.target(name: "App", dependencies: ["Vapor", "LeafProvider"],
+				  exclude: [
+					"Config",
+					"Database",
+					"Public",
+					"Resources"
+			]),
+		.testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+	]
 )
 
--- Sources/Run/main.swift
+++
@@ -1,8 +0,0 @@
-
-let config = try Config()
-try config.setup()
-
-let drop = try Droplet(config)
-
-try drop.setup()
-try drop.run()
