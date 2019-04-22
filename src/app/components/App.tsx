e6e3bcc4f75a7e015724a12f3cb0ccd54a81d860
Roland Leth <roland@leth.ro>
2019-04-22 14:42:20 +0300

    Paddle init

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,14 +1,23 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Route, Switch } from "react-router-dom"
 
 interface Props {
 	ssrLocation?: string
 }
 
-const App = ({ ssrLocation: string }: Props) => (
-	<Switch>
-		<Route exact path="/" render={() => <div>It works</div>} />
-	</Switch>
-)
+const App = ({ ssrLocation }: Props) => {
+	useEffect(() => {
+		// @ts-ignore
+		Paddle.Setup({ vendor: 39394 })
+	}, [])
+
+	return (
+		<>
+			<Switch>
+				<Route exact path="/" render={() => <div>It works</div>} />
+			</Switch>
+		</>
+	)
+}
 
 export default App
