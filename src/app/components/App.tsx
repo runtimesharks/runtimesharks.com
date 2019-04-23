a5b1b12ad408e713313479dd43f4d43e454a1506
Roland Leth <roland@leth.ro>
2019-04-23 17:03:41 +0300

    Improved App hook

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -18,29 +18,37 @@ const App = ({ ssrLocation }: Props) => {
 		// @ts-ignore
 		Paddle.Setup({ vendor: 39394 })
 
+		// Don't animate the text after the first load, it gets annoying.
 		setTimeout(() => setDidAnimate(true), 1100)
 
-		document.addEventListener("scroll", () => {
+		const visible = "visible"
+		const fixed = "fixed"
+
+		const handleScroll = () => {
 			const passedFixedTreshold = window.scrollY > 103
 			const navPadding = document.getElementsByClassName("nav-padding")[0]
 			const nav = document.getElementsByClassName("navigation")[0]
 
 			if (passedFixedTreshold) {
-				if (!navPadding.classList.contains("visible")) {
-					navPadding.classList.add("visible")
+				if (navPadding.classList.contains(visible) === false) {
+					navPadding.classList.add(visible)
 				}
-				if (!nav.classList.contains("fixed")) {
-					nav.classList.add("fixed")
+				if (nav.classList.contains(fixed) === false) {
+					nav.classList.add(fixed)
 				}
-			} else if (!passedFixedTreshold) {
-				if (navPadding.classList.contains("visible")) {
-					navPadding.classList.remove("visible")
+			} else if (passedFixedTreshold === false) {
+				if (navPadding.classList.contains(visible)) {
+					navPadding.classList.remove(visible)
 				}
-				if (nav.classList.contains("fixed")) {
-					nav.classList.remove("fixed")
+				if (nav.classList.contains(fixed)) {
+					nav.classList.remove(fixed)
 				}
 			}
-		})
+		}
+
+		document.addEventListener("scroll", handleScroll)
+
+		return () => document.removeEventListener("scroll", handleScroll)
 	}, [])
 
 	return (
