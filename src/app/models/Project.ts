d23ad381a5c781893bb25aeab0184f715d432083
Roland Leth <roland@leth.ro>
2019-04-26 10:40:13 +0300

    Improved project slug

--- src/app/models/Project.ts
+++ src/app/models/Project.ts
@@ -34,7 +34,7 @@ export default class Project {
 		this.paddleProductId = paddleProductId
 		this.slug = name
 			.toLowerCase()
-			.replace(/[& ,!]/g, "-")
+			.replace(/[& ,!']/g, "-")
 			.replace("--", "-")
 
 		if (this.slug.endsWith("-")) {
