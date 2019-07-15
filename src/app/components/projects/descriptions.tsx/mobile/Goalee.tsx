7d303415d108378c7dc569e185a89ecf36665498
Roland Leth <roland@leth.ro>
2019-07-15 09:51:44 +0300

    Updated packages

--- src/app/components/projects/descriptions.tsx/mobile/Goalee.tsx
+++ src/app/components/projects/descriptions.tsx/mobile/Goalee.tsx
@@ -3,12 +3,12 @@ import Project from "../../../../pages/Project"
 import AnimatedContainer from "../../../AnimatedContainer"
 
 const Goalee = (props: any) => (
-	<Project {...props} centered>
-		<AnimatedContainer delay={0.75}>
-			What drives you each day? What matters to you the most? Remind yourself
-			often, so you don't forget what your true goals in life are!
-		</AnimatedContainer>
-	</Project>
+  <Project {...props} centered>
+    <AnimatedContainer delay={0.75}>
+      What drives you each day? What matters to you the most? Remind yourself
+      often, so you don't forget what your true goals in life are!
+    </AnimatedContainer>
+  </Project>
 )
 
 export default Goalee
