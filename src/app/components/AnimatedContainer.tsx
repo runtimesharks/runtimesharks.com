e9b10231d6540173d4cd94bcc9d06c541387fd5e
Roland Leth <roland@leth.ro>
2019-07-28 19:16:18 +0300

    Added ?? and ?. plugins

--- src/app/components/AnimatedContainer.tsx
+++ src/app/components/AnimatedContainer.tsx
@@ -4,45 +4,43 @@ import styled, { css, keyframes } from "styled-components"
 import InitialAnimationContext from "../utils/InitialAnimationContext"
 
 interface StyleProps {
-	shouldAnimate: boolean
-	delay: number
+  shouldAnimate: boolean
+  delay: number
 }
 
 interface Props {
-	delay?: number
-	position?: number
-	disableAnimations?: boolean
-	sidepaded?: boolean
-	children?: any
+  delay?: number
+  position?: number
+  disableAnimations?: boolean
+  sidepaded?: boolean
+  children?: any
 }
 
 function AnimatedContainer({
-	delay = AnimatedContainer.baseDelay,
-	position = 1,
-	disableAnimations = false,
-	sidepaded = true,
-	children,
-	history,
+  delay = AnimatedContainer.baseDelay,
+  position = 1,
+  disableAnimations = false,
+  sidepaded = true,
+  children,
+  history,
 }: Props & RouteComponentProps) {
-	const didAnimate = useContext(InitialAnimationContext)
-	const isPop = history.action === "POP"
-	// On `PUSH`, `didAnimate` will be `true` -> not first render.
-	// On `POP` [back | forward button], `didAnimate` will be `true` -> not first render.
-	// On `POP` first render, `didAnimate` will be `false` -> first render.
-	const isFirstRender = didAnimate === false || isPop === false
-	const shouldAnimate = disableAnimations === false && isFirstRender
+  const didAnimate = useContext(InitialAnimationContext)
+  const isPop = history.action === "POP"
+  // On `PUSH`, `didAnimate` will be `true` -> not first render.
+  // On `POP` [back | forward button], `didAnimate` will be `true` -> not first render.
+  // On `POP` first render, `didAnimate` will be `false` -> first render.
+  const isFirstRender = didAnimate === false || isPop === false
+  const shouldAnimate = disableAnimations === false && isFirstRender
 
-	console.log(history.action)
-
-	return (
-		<Container
-			className={`${sidepaded ? "side-padded" : null}`}
-			shouldAnimate={shouldAnimate}
-			delay={position * delay}
-		>
-			{children}
-		</Container>
-	)
+  return (
+    <Container
+      className={`${sidepaded ? "side-padded" : null}`}
+      shouldAnimate={shouldAnimate}
+      delay={position * delay}
+    >
+      {children}
+    </Container>
+  )
 }
 
 const appearFromBelow = keyframes`
@@ -58,14 +56,14 @@ const appearFromBelow = keyframes`
 `
 
 const Container = styled.div<StyleProps>`
-	${(props: StyleProps) =>
-		props.shouldAnimate
-			? css`
-					opacity: 0;
-					animation: ${appearFromBelow} 0.75s ${props.delay}s forwards
-						ease-in-out;
-			  `
-			: null}
+  ${(props: StyleProps) =>
+    props.shouldAnimate
+      ? css`
+          opacity: 0;
+          animation: ${appearFromBelow} 0.75s ${props.delay}s forwards
+            ease-in-out;
+        `
+      : null}
 `
 
 AnimatedContainer.baseDelay = 0.25
