21d5bcc81394fb56bd21f50bd2360dceaa069c28
Roland Leth <roland@leth.ro>
2019-07-31 18:49:21 +0300

    Project stuff fixed

--- razzle.config.js
+++ razzle.config.js
@@ -1,27 +1,29 @@
 module.exports = {
-	modify(defaultConfig) {
-		// modify(defaultConfig, { target, dev }, webpack) {
-		const config = defaultConfig // stay immutable here
-		config.resolve.extensions.push(".ts", ".tsx")
-		config.module.rules.push({
-			test: /\.(ts|js)x?$/,
-			exclude: [/node_modules/, /build/],
-			loader: "babel-loader",
-		})
-		return config
-	},
-	// plugins: [
-	// 	{
-	// 		name: "typescript",
-	// 		options: {
-	// 			useBabel: true,
-	// 			forkTsChecker: {
-	// 				tslint: false,
-	// 				typeCheck: false,
-	// 				ignoreLintWarnings: true,
-	// 				silent: true,
-	// 			},
-	// 		},
-	// 	},
-	// ],
+  // This doesn't properly work; for example, constructor properties
+  // are not working, even with plugins and presets added ¯\-(ツ)-/¯
+  // modify(defaultConfig) {
+  // 	// modify(defaultConfig, { target, dev }, webpack) {
+  // 	const config = defaultConfig // stay immutable here
+  // 	config.resolve.extensions.push(".ts", ".tsx")
+  // 	config.module.rules.push({
+  // 		test: /\.(ts|js)x?$/,
+  // 		exclude: [/node_modules/, /build/],
+  // 		loader: "babel-loader",
+  // 	})
+  // 	return config
+  // },
+  plugins: [
+    {
+      name: "typescript",
+      options: {
+        useBabel: true,
+        forkTsChecker: {
+          tslint: false,
+          typeCheck: false,
+          ignoreLintWarnings: true,
+          silent: true,
+        },
+      },
+    },
+  ],
 }
