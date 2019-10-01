fc3c41c312a3c65de5c1908e50404625773157b7
Roland Leth <roland@runtimesharks.com>
2019-10-01 11:33:56 +0300

    Removed some text from ContactUs

--- src/app/components/projects/ContactUs.tsx
+++ src/app/components/projects/ContactUs.tsx
@@ -2,26 +2,23 @@ import React from "react"
 import styled from "styled-components"
 
 const ContactUs = () => (
-	<Container className="centered">
-		We're focused on mobile applications, but we also love building web apps;{" "}
-		<Spacer />
-		if you'd like to get in touch, you can reach us at{" "}
-		<a href="mailto:contact@runtimesharks.com">
-			contact@runtimesharks.com
-		</a>. <Spacer />
-		Please find below some of our projects and past work.
-	</Container>
+  <Container className="centered">
+    We're focused on mobile applications, but we also love building web apps;{" "}
+    <Spacer />
+    if you'd like to get in touch, you can reach us at{" "}
+    <a href="mailto:contact@runtimesharks.com">contact@runtimesharks.com</a>.
+  </Container>
 )
 
 const Container = styled.div`
-	max-width: 700px;
-	margin: auto;
+  max-width: 700px;
+  margin: auto;
 `
 
 const Spacer = styled.br`
-	@media screen and (max-width: 630px) {
-		display: none;
-	}
+  @media screen and (max-width: 630px) {
+    display: none;
+  }
 `
 
 export default ContactUs
