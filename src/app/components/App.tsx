940ee6a56e017a778cad7e501d2a2d26f28bcca8
Roland Leth <roland@runtimesharks.com>
2019-10-28 19:00:27 +0200

    Removed navigation scroll handler

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -13,37 +13,6 @@ const App = ({ ssrLocation }: Props) => {
   useEffect(() => {
     // @ts-ignore
     Paddle.Setup({ vendor: 39394 })
-
-    const visible = "visible"
-    const fixed = "fixed"
-
-    const handleScroll = () => {
-      const passedFixedTreshold = window.scrollY > 103
-      const navPadding = document.getElementsByClassName("nav-padding")[0]
-      const nav = document.getElementsByClassName("navigation")[0]
-
-      if (passedFixedTreshold) {
-        if (navPadding.classList.contains(visible) === false) {
-          navPadding.classList.add(visible)
-        }
-
-        if (nav.classList.contains(fixed) === false) {
-          nav.classList.add(fixed)
-        }
-      } else if (passedFixedTreshold === false) {
-        if (navPadding.classList.contains(visible)) {
-          navPadding.classList.remove(visible)
-        }
-
-        if (nav.classList.contains(fixed)) {
-          nav.classList.remove(fixed)
-        }
-      }
-    }
-
-    document.addEventListener("scroll", handleScroll)
-
-    return () => document.removeEventListener("scroll", handleScroll)
   }, [])
 
   return (
