edcae1e0b72e9e32f06237fa162b08bd3a9f1087
Roland Leth <roland@leth.ro>
2020-03-04 11:23:44 +0200

    Removed privacy policy and terms for TS

--- src/app/pages/PrivacyPolicy.tsx
+++ src/app/pages/PrivacyPolicy.tsx
@@ -6,8 +6,6 @@ import ChallengeBeat from "../components/privacy-policy/ChallengeBeat"
 import Goalee from "../components/privacy-policy/Goalee"
 import IdeaTrackerr from "../components/privacy-policy/IdeaTrackerr"
 import RuntimeSharks from "../components/privacy-policy/RuntimeSharks"
-import TravelStories from "../components/privacy-policy/TravelStories"
-import TravelStoriesTerms from "../components/terms-and-conditions/TravelStories"
 import Helmet from "../utils/Helmet"
 
 const PrivacyPolicy = () => (
@@ -20,13 +18,8 @@ const PrivacyPolicy = () => (
         <Switch>
           <Route path="/privacy/runtimesharks" component={RuntimeSharks} />
           <Route path="/privacy/challengebeat" component={ChallengeBeat} />
-          <Route path="/privacy/travelstories" component={TravelStories} />
           <Route path="/privacy/goalee" component={Goalee} />
           <Route path="/privacy/idea-tracker" component={IdeaTrackerr} />
-          <Route
-            path="/terms-and-conditions/travelstories"
-            component={TravelStoriesTerms}
-          />
         </Switch>
       </Content>
     </AnimatedContainer>
